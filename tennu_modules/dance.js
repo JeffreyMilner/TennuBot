1a3dee66c65ea7d1d9f01210594a63a63a951432
JeffreyMilner <jeffrey.milner@email.wsu.edu>
2013-07-18 20:14:05 -0700

    Removed the dance module since it ended up not being used from there

--- tennu_modules/dance.js
+++
@@ -1,9 +0,0 @@
-module.exports = function TennuSayActModule (tennu) {
-
-    if(/dance/.test(command.args.join(' ').toLowerCase())) {
-        tennu.say(command.args[0], command.args.slice(1).join(' '));
-    }
-
-    return {
-    };
-};
