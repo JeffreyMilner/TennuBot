d2bcf40c582e929bec2436666e00055e99593e77
JeffreyMilner <jeffrey.milner@email.wsu.edu>
2013-07-21 10:41:31 -0700

    Updated the roulette module's output

--- tennu_modules/roulette.js
+++ tennu_modules/roulette.js
@@ -12,12 +12,22 @@ module.exports = function TennuRouletteModule (tennu) {
         var bullet = chambers.shift();
         var output = "chamber " + (6 - chambers.length) + " of 6 => ";
 
-        if(bullet) {
-            tennu.say(command.channel, output + "*BANG*");
-            tennu.say(command.channel, command.sender + " has been shot");
-            tennu.say(command.channel, reload(), true);
-        } else {
-            tennu.say(command.channel, output + "+click+");
+        if((command.args[0] == "" || command.args[0] == null)) {
+            if(bullet) {
+                tennu.act(command.channel, "shoots " + output + "*BANG*");
+                tennu.say(command.channel, command.sender + " has been shot");
+                tennu.act(command.channel, reload(), true);
+            } else {
+                tennu.act(command.channel, "shoots " + output + "+click+");
+            }
+        } else if(command.args[0].match(/#\w+/g)) {
+            if(bullet) {
+                tennu.act(command.args[0], "shoots " + output + "*BANG*");
+                tennu.say(command.args[0], command.sender + " has been shot");
+                tennu.act(command.args[0], reload(), true);
+            } else {
+                tennu.act(command.args[0], "shoots " + output + "+click+");
+            }
         }
     };
     reload();
